65625b7ad979d855cdfa7967dffe51100955f826
mavaldot <48870467+mavaldot@users.noreply.github.com>
2023-07-19 11:50:30 -0500

    Finish ch5 challenges

--- ch5/challenges.sh
+++ ch5/challenges.sh
@@ -10,10 +10,16 @@ done
 
 #Compare natural numbers and display "X is greater than Y", "X is equal to Y" or "X is less than Y".
 
-read -p "Enter a natural number: " x
-read -p "Enter another natural number: " y
-res=$(( x + y ))
-echo "The sum is $res"
+read -p "Enter x: " x
+read -p "Enter y: " y
+
+if [[ x -gt y ]]; then
+	echo "X is greater than Y"
+elif [[ x -lt y ]]; then
+	echo "X is less than Y"
+else
+	echo "X is equal to Y"
+fi
 
 #Read N numbers from the stdio and compute their average.
 
@@ -52,9 +58,19 @@ echo "Current time: $t, Current date: $d"
 
 #Write a script that prints how many parameters are passed and which ones? For example, if I run the script like this: ./script1 p1 p2 p3 p4, then it should print: 4 params were passed and they're these ones: p1, p2, p3, p4
 
-
+echo "$# arguments were passed and they're these ones: $@"
 
 #Write a script that prints information about your OS and version, release number, and kernel version.
 
+uname -sr
+lsb_release -d
+uname -v
 
 #Write a script that checks if cups service is running. If it running, print "Cups' status is running". Otherwise, prints "Cups's status is stopped".
+
+ps aux | grep -v grep | grep cups 1>/dev/null 2>/dev/null
+if [ $? -eq 0 ]; then
+	echo "Cups's status is running"
+else
+	echo "Cups's status is stopped"
+fi
